cf63b6a6f57a8ec91fac41061bba4f68d462cb6c
pearl1991 <pearlmcphee@gmail.com>
2015-08-14 15:31:07 -0400

    added solution to problems

--- finalReview/Problem2/problem2.js
+++ finalReview/Problem2/problem2.js
@@ -6,10 +6,10 @@ jQuery has been imported for you.
 A client has contacted you because they've written terrible JavaScript.
 Repair their code! There are 10 bugs in the code. Make a short description of what you've fixed.
 
-1.  ?
-2.  ?
-3.  ?
-4.  ?
+1.  added single quotes to css method in doc ready function
+2.  added () after function in doc ready function
+3.  deleted period from ul bc not a class
+4.  deleted period from p tag bc not a class
 5.  ?
 6.  ?
 7.  ?
@@ -18,8 +18,8 @@ Repair their code! There are 10 bugs in the code. Make a short description of wh
 10. ?
 */
 
-document.ready(function {
-    $('nav .ul li').css('list-style-type' square);
+document.ready(function (){
+    $('nav ul li').css('list-style-type', 'square');
 
-    $('.output').text('.p'.text.split( ).length());
+    $('.output').text('p'.text.split( ).length());
 });
